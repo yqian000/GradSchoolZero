ec0584ee9c698330aac52751dab4a66df1fec765
Yue Qian <yueqian1110@gmail.com>
2021-10-20 16:40:11 -0400

    Create basic templates for students.
    Add skeleton code for student features.
    
    View the newly created page at url:
    studentView/
    rateClass/
    fileComplaint/

---
+++ mysite/student/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('studentView/', views.studentView, name='studentView'),
+	path('rateClass/', views.rateClass, name='rateClass'),
+	path('fileComplaint/', views.fileComplaint, name='fileComplaint'),
+]
