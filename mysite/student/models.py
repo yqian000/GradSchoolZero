e2cf0a5725b6afdf880ab97ae0b69da0571854a8
Yue Qian <yueqian1110@gmail.com>
2021-11-04 18:22:27 -0400

    Fix typos and html styling.
    Create userbase.html which is the base.html with a side nav bar. All user html files should extend userbase.html instead of base.html

--- mysite/student/models.py
+++ mysite/student/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 
-<<<<<<< Updated upstream
 # Create your models here.
-=======
-class Applcation(models.Model):
+class Application(models.Model):
     email=models.EmailField()
     firstname=models.CharField(max_length=150,blank="True")
     lastname=models.CharField(max_length=150,blank="True")
-    Gpa=models.CharField(max_length=150,blank="True")
+    gpa=models.CharField(max_length=150,blank="True")
     semester=models.CharField(max_length=150,blank="True")
-    Birthday=models.DateField()
+    birthday=models.DateField()
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60, default="NewYork")
     state = models.CharField(max_length=30, default="NewYork")
     zip = models.CharField(max_length=5, default="11220")
     country = models.CharField(max_length=50)
-    transcprit= models.FileField(upload_to='student/documents/')
+    transcript= models.FileField(upload_to='student/documents/')
     letters= models.FileField(upload_to='student/documents/')
     personal_statement = models.FileField(upload_to='student/documents/')
     major=models.CharField(max_length=150,blank="True")
- 
-
-
->>>>>>> Stashed changes
