d9a0a853ef3527d2f17390a1c1c21579e3def5db
chuantianlin <45149972+chuantianlin@users.noreply.github.com>
2021-11-11 12:23:28 -0500

    only register can view the userpage and it can list all the application on the userview page. Also add a forbbiden html, we should all use that page if you don't allow people to view certain pages DRY

--- mysite/account/models.py
+++ mysite/account/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9928459720586b393e85fdd26c4e410a75810ad17c46e09b97129936a2f901e
-size 2673
+oid sha256:9a0ad16e519f3746c636b8303ac8cd4d09b13ae4a6f16adff789f45f5a942d23
+size 2741
