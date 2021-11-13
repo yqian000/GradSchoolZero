c287b652b2bc59d24a52d46b7209a79d44fa55c8
Yue Qian <yueqian1110@gmail.com>
2021-11-13 18:24:56 -0500

    Clean up

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-
-
-# Create your models here.
-class Student(models.Model):
-	name = models.CharField(max_length=60)
-
-	def __str__(self):
-		return self.name
-
-		
