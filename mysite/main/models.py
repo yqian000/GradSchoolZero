acf4be63386f172df2de52d3055919f9c6c24cc9
Yue Qian <yueqian1110@gmail.com>
2021-11-13 16:13:05 -0500

    Update models.py

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:641a73c8b7bd8e0a81e2ff54e8dc5b72a5fc57461fee8bd1dd88e8ce2fde3f26
-size 235
+oid sha256:d025c352a20b5bdc9c51a93e1befbcf2015aae3b67c226e435b0de9b249f8d52
+size 343
