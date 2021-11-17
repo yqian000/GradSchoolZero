a28554c9091109afefd2a809e3e916187974a03b
chuantianlin <45149972+chuantianlin@users.noreply.github.com>
2021-11-17 11:26:47 -0500

    ...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -148,6 +148,6 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'llcttt@gmail.com'
+EMAIL_HOST_USER = 'chuantian.lin@cpustudio.com'
 EMAIL_HOST_PASSWORD = 'Lct960718?'
 CRISPY_TEMPLATE_PACK="bootstrap4"
