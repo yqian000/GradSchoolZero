3fa2da3ce684a41f95ddc9b44508207a89562881
Yue Qian <63725755+yqian000@users.noreply.github.com>
2021-12-02 14:43:05 -0500

    feature 1

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,6 +3,21 @@ from account.models import *
 
 # Create your views here.
 def home(request):
-	return render(request, "main/home.html", {})
+	students = Student.objects.all()
+	courses = Course.objects.all()
+	max_rate = 0
+	max_gpa = 0
+
+	for student in students:
+		if student.GPA > max_gpa:
+			max_gpa = student.GPA
+	top_students = Student.objects.filter(GPA=max_gpa)
+
+	for course in courses:
+		if course.rate > max_rate:
+			max_rate = course.rate
+	top_classes = Course.objects.filter(rate=max_rate)
+
+	return render(request, "main/home.html", {'ts': top_students, 'tc': top_classes, 'c':courses})
 
 
